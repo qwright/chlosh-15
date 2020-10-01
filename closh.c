6000a9df803ce2ba098f1ece8dbf7cf7dd9e70ed
quinn <qfwright@outlook.com>
2020-10-01 11:18:15 -0700

    correct number of runs and sequential processing

--- closh.c
+++ closh.c
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -37,6 +39,8 @@ int main() {
 		
 		pid_t pid; //parent id
 		int status; //status of process
+		time_t start, final;
+		int final_time;
     
     while (TRUE) { // main shell input loop
         
@@ -71,15 +75,35 @@ int main() {
 
 				//loop for child processes as specified by count
 				while(count > 1){
-					pid = fork();
+					pid = fork();//literally think of this as a fork in the road, two processes will execute, child gets 0, parent gets child id
+					time(&start);//start a timer for each proccess
 					if(pid==0){
-						execvp(cmdTokens[0], cmdTokens);
+						printf("CHILD PID: %d, Creating Process...\n", pid);
+						execvp(cmdTokens[0], cmdTokens);//this ends the current scope
 					}
+					//if process is sequential, wait for child to wrap up.
+					if(parallel==0){
+						printf("Waiting for Child to wrap up...\n");
+						pid = waitpid(pid, &status, WUNTRACED); //we use untraced as the child argument is in a different execution context	...I think ;)
+						printf("Child process ended.\n");
+					}
+					sleep(3); //for testing timeout
+					time(&final);//final timer
+					final_time = final - start;
+					printf("time: %d\n", final_time);
+
+					if(timeout !=0 && final_time > timeout){
+						printf("Killing child process\n");
+						kill(pid, SIGKILL);
+					}
+					
+					printf("Parent PID: %d, Keeping track of Child process...\n", pid); //dont execvp, as it would end program
 					count--;
 				}
 				//parent executes here i.e. final count
+				printf("Not a forked process, Creating Final Process...\n");
 				execvp(cmdTokens[0], cmdTokens);
-
+				
         // doesn't return unless the calling failed
         printf("Can't execute %s\n", cmdTokens[0]); // only reached if running the program failed
         exit(1);        
